ceb1ac10246751141974f4d8b1c62cff6b8ad205
CKRLMMDW <58661173+CKRLMMDW@users.noreply.github.com>
2019-12-10 17:28:37 +0000

    Add files via upload

--- Sprint 2/Pass/disavg2.php
+++ Sprint 2/Pass/disavg2.php
@@ -8,6 +8,7 @@
 <thead><tr>
    <th><strong>Student ID</strong></th>
    <th><strong>CW ID</strong></th>
+   <th><strong>Actual Mark</strong></th>
 </thead></tr>
 
 <!-- START PHP -->
@@ -20,17 +21,28 @@ $link = mysqli_connect("localhost", "root", "", "gadb");
 }
 $sql = "SELECT * FROM scw WHERE sid='$sid'";
 $result = mysqli_query($link,$sql);
+
+$rt=0;   // Running total
+$i=0;    // Counter
+
+
 while($row = mysqli_fetch_assoc($result)){
    echo "<tr>";
    echo "<td>" .$row['sid']."</td>";
-	 echo "<td>" .$row['cwid']."</td>";
-	 echo "</tr>";
+   echo "<td>" .$row['cwid']."</td>";
+   echo "<td>" .$row['ma']."</td>";
+   echo "</tr>";
+   $rt=$rt+$row['ma'];
+   $i++;
 }
 mysqli_free_result($result);       //free memory
 mysqli_close($link);               //close connection
 
-
-	  echo "<br>Average: <br>";
+   $avg=$rt/$i;
+   
+	  echo "<br>Average: ";
+      echo $avg;
+      echo "%<br>";
 	  echo "<a href='menu.html'><br>Return to Menu</a>";  
 
 
