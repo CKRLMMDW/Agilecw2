17bc40f8846cdb820088f03bf4a67b4ec37bf2fe
CKRLMMDW <58661173+CKRLMMDW@users.noreply.github.com>
2019-12-10 16:34:00 +0000

    Add files via upload

--- Sprint 2/Refactored/dbinfo.php
+++ Sprint 2/Refactored/dbinfo.php
@@ -1,19 +1,17 @@
  <?php
- // local testing 
+ // local testing
  $dbhost="localhost";   // Database Host
- $dbun="root";		// Username
- $dbpw="";		// Password
- $dbname="gadb";	// Database Name
- 
+ $dbun="root";	      	// Username
+ $dbpw="";	          	// Password
+ $dbname="gadb";	      // Database Name
+
     // $dbhost="";
     // $dbun="";
     // $dbpw="";
     // $dbname="";
-	
+
  // in php code file
- 
-    // require_once('dbinfo.php'); 
+
+    // require_once('dbinfo.php');
     // $link = mysqli_connect($dbhost, $dbun, $dbpw, $dbname);
  ?>
- 
- 
