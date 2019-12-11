11bc703e742c0de40f71d847c157e0df520992d4
CKRLMMDW <58661173+CKRLMMDW@users.noreply.github.com>
2019-12-11 12:41:49 +0000

    Add files via upload

--- Sprint 2/Refactored/disavg2.php
+++ Sprint 2/Refactored/disavg2.php
@@ -45,11 +45,32 @@ while($row = mysqli_fetch_assoc($result)){
 mysqli_free_result($result);       //free memory
 mysqli_close($link);               //close connection
 
-   $avg=$rt/$i; // avg = total / counter 
+   $p=$rt/$i; // avg = total / counter 
 
 	 echo "<br>Average: ";
-   echo $avg;
-   echo "%";
+   echo $p;
+   echo "%   ";
+   switch($p){
+	case $p > 94 :	   echo "A1  <br>";	   break;
+	case $p > 88 :	   echo "A2  <br>";	   break;
+	case $p > 82 :	   echo "A3  <br>";	   break;
+	case $p > 75 :	   echo "A4  <br>";	   break;
+	case $p > 69 :	   echo "A5  <br>";	   break;
+	case $p > 66 :	   echo "B1  <br>";	   break;
+	case $p > 63 :	   echo "B2  <br>";	   break;
+	case $p > 59 :	   echo "B3  <br>";	   break;
+	case $p > 56 :	   echo "C1  <br>";	   break;
+	case $p > 53 :	   echo "C2  <br>";	   break;
+	case $p > 49 :	   echo "C3  <br>";	   break;
+	case $p > 46 :	   echo "D1  <br>";	   break;
+	case $p > 43 :	   echo "D2  <br>";	   break;
+	case $p > 39 :	   echo "D3  <br>";	   break;
+	case $p > 36 :	   echo "MF1 <br>";	   break;
+	case $p > 33 :	   echo "MF2 <br>";	   break;
+	case $p > 29 :	   echo "MF3 <br>";	   break;
+	case $p > 19 :	   echo "CF  <br>";	   break;
+	case $p > 0 :	   echo "BF  <br>";	   break;	   
+} 
    echo "<br>";
 	 echo "<a href='menu.html'><br>Return to Menu</a>";
 ?>
